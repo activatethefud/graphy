f1ed5ab88ac4bd4f95b9249c2d3d2d9a10563068
nsutic <nikolasutic1@gmail.com>
2019-05-31 16:46:31 +0200

    Improved generator script.

--- graph_generator.sh
+++ graph_generator.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-1=1
+for iterator in $(seq 1 $1); do
+	echo -e "$iterator\c"
 
-for iterator in $(seq $1 $2); do
-	echo -e "$iterator \c"
-
-	for _ in $(seq $1 $(($2-1))); do
-		echo -e "$(($RANDOM%$2)) \c"
+	for _ in $(seq 1 $(($1-1))); do
+		echo -e " $(($RANDOM%$1+1))\c"
 	done
 
 	echo ""
 done
+
+# Example output to pipe to input.txt
+# 1 5 3 3 8 6 6 0 4 7 
+# 2 1 9 8 3 2 1 1 2 4 
+# 3 4 9 7 6 6 4 8 8 0 
+# 4 9 0 7 7 4 1 3 4 0 
+# 5 9 6 1 9 6 5 8 2 7 
+# 6 4 4 0 4 5 5 1 0 4 
+# 7 0 1 9 4 1 7 1 6 5 
+# 8 3 8 4 7 9 8 9 0 0 
+# 9 5 7 9 0 0 4 9 7 2 
+# 10 2 6 0 7 5 0 9 1 7 
