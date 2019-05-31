18596a4feb947b81c3c8f215134f66e12642b720
nsutic <nikolasutic1@gmail.com>
2019-05-31 16:29:46 +0200

    Nodes now in circular positions.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from graphics import *
 from random import *
+from time import sleep
+import math
 import sys
+
+node_count=0
+
+def generate_circle_coord(center,radius):
+	# Generate node coordinates to be in a circle (TODO Increase space between nodes)
+	t=randint(0,node_count**5)
+	if randint(0,1) is True:
+		t+=0.25
+	x=center+radius*math.sin(t)
+	y=center+radius*math.cos(t)
+	return (x,y)
 
 def main():
 
-	# Initiliaze the grap and graphical window
+	# Initiliaze the graph and graphical window
+	width=400
+	height=400
 	graph={}
-	graph_win=GraphWin("Graph",200,200)
+	graph_win=GraphWin("Graph",width,height)
 	graph_win.setBackground("white")
 
 	input_file=open("input.txt","r");
@@ -15,33 +30,64 @@ def main():
 	for line in input_file.readlines():
 		line=line.rstrip('\n')
 		graph[line.split(' ')[0]]=line.split(' ')[1::]
+		global node_count
+		node_count+=1
 
-	# Draw all nodes inputted (TODO)
+	# Radius in which all nodes will be in (in px)
+	circle_radius=width*0.75/2
+
+	# The shift of node label from node center
+	label_shift=7
+
+	# Draw all nodes inputted
 	node_points={}
 	for vertex in graph.keys():
+
 		if vertex not in node_points.keys():
-			pt=Point(randint(0,200),randint(0,200))
-			label=Text(pt,str(vertex))
+			coords=generate_circle_coord(width/2,circle_radius)
+			pt=Point(coords[0],coords[1])
+
+			if coords[0] < width/2:
+				lpt=Point(coords[0]-label_shift,coords[1])
+			else:
+				lpt=Point(coords[0]+label_shift,coords[1])
+
+			label=Text(lpt,str(vertex))
 			label.draw(graph_win)
+
 			node_points[vertex]=pt
 		
 		for neighbour in graph[vertex]:
 			if neighbour not in node_points.keys():
-				pt=Point(randint(0,200),randint(0,200))
-				label=Text(pt,str(neighbour))
+				coords=generate_circle_coord(width/2,circle_radius)
+				pt=Point(coords[0],coords[1])
+
+				if coords[0] < width/2:
+					lpt=Point(coords[0]-label_shift,coords[1])
+				else:
+					lpt=Point(coords[0]+label_shift,coords[1])
+					
+				label=Text(lpt,str(neighbour))
 				label.draw(graph_win)
+
 				node_points[neighbour]=pt
 	
-	# Draw the edges
+	# Draw the edges, skip edges already drawn
+	edges=[]
+	visited=[]
 	for vertex in graph.keys():
 		
+		if vertex not in visited:
+			visited.append(vertex)
+		
 		for neighbour in graph[vertex]:
-			edge=Line(node_points[vertex],node_points[neighbour])
-			edge.draw(graph_win)
+			if vertex not in visited or neighbour not in visited:
+				edge=Line(node_points[vertex],node_points[neighbour])
+				len(sys.argv) != 1 and sleep(int(sys.argv[1]))
+				edge.draw(graph_win)
 	
 	# Pause the graphical window, awaiting mouse input
 	graph_win.getMouse()
-	print(graph)
 
 	input_file.close()
 
